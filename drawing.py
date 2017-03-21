aa405f6d8454b61dfa554c9e315f3f3eae9fbd43
zhangshuimu <765732980@qq.com>
2017-03-21 19:36:39 +0800

    update

--- drawing.py
+++ drawing.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
-import subprocess
 import json
 import uuid
 from flask import Flask, jsonify
+from scrapy.crawler import CrawlerProcess
+from draw.spiders.a163 import A163Spider
 
 
 app = Flask(__name__)
@@ -19,6 +20,12 @@ def fetch():
 	# subprocess.check_output(['scrapy', 'crawl', 'a163', "-o", tmp_file])
 	# with open(tmp_file) as items_file:
 	# 	return jsonify(json.load(items_file)[0])
+	settings = {
+		'USER_AGENT': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_3) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.54 Safari/536.5',
+	}
+	process = CrawlerProcess(settings)
+	process.crawl(A163Spider)
+	process.start(stop_after_crawl=True)
 	return 'DONE!'
 
 
