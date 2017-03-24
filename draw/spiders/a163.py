e7812f2ca4cd2b846121ae08dd9b7caf7e95e82a
mike <765732980@qq.com>
2017-03-24 18:54:55 +0800

    fixed

--- draw/spiders/a163.py
+++ draw/spiders/a163.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import scrapy
+from scrapy import signals
 import re
 from draw.items import DrawingItem
 
@@ -9,6 +10,20 @@ class A163Spider(scrapy.Spider):
     name = "a163"
     allowed_domains = ["caipiao.163.com"]
     start_urls = ['http://caipiao.163.com/order/3d/#from=syks']
+
+    @classmethod
+    def from_crawler(cls, crawler, *args, **kwargs):
+        spider = super(A163Spider, cls).from_crawler(crawler, *args, **kwargs)
+        crawler.signals.connect(spider.spider_closed, signals.spider_closed)
+        return spider
+
+
+    def spider_closed(self, spider):
+    	print('----------------close------------', spider.name)
+    	self.running_crawlers.remove(spider)
+    	if not self.running_crawlers:
+    		reactor.stop()
+
 
     def parse(self, response):
         item = DrawingItem()
--- drawing.py
+++ drawing.py
@@ -4,30 +4,86 @@ import uuid
 from flask import Flask, jsonify
 from scrapy.crawler import CrawlerProcess
 from draw.spiders.a163 import A163Spider
+from scrapy.utils.project import get_project_settings
+from twisted.internet import reactor
+from scrapy.crawler import CrawlerRunner
+from celery import Celery
+from celery.schedules import crontab
+
+
+def make_celery(app):
+    celery = Celery(app.import_name, broker=app.config['celery_broker_url'])
+    celery.conf.update(app.config)
+    celery.conf.timezone = 'Asia/Hong_Kong'
+    celery.conf.imports = 'drawing'
+    TaskBase = celery.Task
+
+    class ContextTask(TaskBase):
+        abstract = True
+
+        def __call__(self, *args, **kwargs):
+            with app.app_context():
+                return TaskBase.__call__(self, *args, **kwargs)
+
+    celery.Task = ContextTask
+    return celery
 
 
 app = Flask(__name__)
+app.config.update(
+    celery_broker_url='redis://localhost:6379/5'
+)
+celery = make_celery(app)
+
+
+@celery.task
+def add_together(a, b):
+    return a + b
+
+
+@celery.task()
+def hello():
+    print('-------------------hello ==============')
+
+
+celery.conf.beat_schedule = {
+    'hello-every-3-minute': {
+        'task': 'drawing.hello',
+        'schedule': crontab(minute='*/3'),
+    },
+}
 
 
 @app.route('/')
 def index():
-	return jsonify({'message': 'hello lambda!'})
+    return jsonify({'message': 'hello lambda!'})
+
+
+@app.route('/add')
+def add():
+    add_together.delay(23, 42)
+    return jsonify({'message': 'hello celery!'})
 
 
 @app.route('/fetch')
 def fetch():
-	# tmp_file = '/tmp/{}-fetch-output.json'.format(uuid.uuid4())
-	# subprocess.check_output(['scrapy', 'crawl', 'a163', "-o", tmp_file])
-	# with open(tmp_file) as items_file:
-	# 	return jsonify(json.load(items_file)[0])
-	settings = {
-		'USER_AGENT': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_3) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.54 Safari/536.5',
-	}
-	process = CrawlerProcess(settings)
-	process.crawl(A163Spider)
-	process.start(stop_after_crawl=True)
-	return 'DONE!'
+    # tmp_file = '/tmp/{}-fetch-output.json'.format(uuid.uuid4())
+    # subprocess.check_output(['scrapy', 'crawl', 'a163', "-o", tmp_file])
+    # with open(tmp_file) as items_file:
+    # 	return jsonify(json.load(items_file)[0])
+
+    # os.environ.setdefault('SCRAPY_SETTINGS_MODULE', 'draw.settings')
+    # process = CrawlerProcess(get_project_settings())
+    # process.crawl(A163Spider)
+    # process.start(stop_after_crawl=True)
+
+    runner = CrawlerRunner(get_project_settings())
+    d = runner.crawl("a163")
+    d = runner.join()
+    d.addBoth(lambda _: reactor.stop())
+    reactor.run()
+    return 'DONE!'
 
 
 if __name__ == '__main__':
-	app.run()
+    app.run()
